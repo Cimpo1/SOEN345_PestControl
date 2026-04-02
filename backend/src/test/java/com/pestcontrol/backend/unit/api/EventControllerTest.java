b0b3ceb49729d986d1815751f24ac266efc8d60b
Cimpo1 <125995046+Cimpo1@users.noreply.github.com>
2026-04-01 20:35:08 -0400

    FIX: remove unused lib

--- backend/src/test/java/com/pestcontrol/backend/unit/api/EventControllerTest.java
+++ backend/src/test/java/com/pestcontrol/backend/unit/api/EventControllerTest.java
@@ -18,7 +18,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
